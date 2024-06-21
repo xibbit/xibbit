cd1cee5bf2feb26eaae888d1bfeaebe456fb4a87
Dan Howard <code_dh@svexpertise.com>
2024-06-21 12:24:18 -0700

    Update flutter_app client build env to current

--- client/flutter_app/android/app/src/main/kotlin/com/example/flutter_public_figure/MainActivity.kt
+++ client/flutter_app/android/app/src/main/kotlin/com/example/flutter_public_figure/MainActivity.kt
@@ -1,6 +1,5 @@
-package com.example.flutter_public_figure
+package com.example.flutter_app
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
